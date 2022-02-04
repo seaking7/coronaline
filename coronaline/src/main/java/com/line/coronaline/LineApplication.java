7c90265b622c38eedeeded6c545cb6fc2ebb41f1
taekyung <taekyung@lguplus.co.kr>
2022-02-04 12:16:27 +0900

    Initial commit

---
+++ coronaline/src/main/java/com/line/coronaline/LineApplication.java
@@ -0,0 +1,13 @@
+package com.line.coronaline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineApplication.class, args);
+	}
+
+}
---
+++ coronaline/src/test/java/com/line/coronaline/CoronalineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.line.coronaline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronalineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
