6e39ffc21b56f912eda5d4b52bfe6e6addec5f23
taekyung <taekyung@lguplus.co.kr>
2022-02-05 17:32:09 +0900

    controller테스트 보완

---
+++ coronaline/src/test/java/com/line/coronaline/controller/api/APIPlaceControllerTest.java
@@ -0,0 +1,44 @@
+package com.line.coronaline.controller.api;
+
+import com.line.coronaline.controller.error.BaseErrorController;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(APIPlaceController.class)
+class APIPlaceControllerTest {
+
+
+    private final MockMvc mvc;
+
+    public APIPlaceControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[API][GET] 장소 리스트 조회")
+    @Test
+    void testGetPlaces() throws Exception {
+        //Given
+
+
+        //When & Then
+        mvc.perform(get("/api/places"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andDo(print());
+
+        //Then
+
+
+    }
+
+}
--- coronaline/src/test/java/com/line/coronaline/controller/error/BaseErrorControllerTest.java
+++ coronaline/src/test/java/com/line/coronaline/controller/error/BaseErrorControllerTest.java
@@ -1,45 +1,38 @@
-package com.line.coronaline.controller;
+package com.line.coronaline.controller.error;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
-@SpringBootTest
-class BaseControllerTest {
-
+@WebMvcTest(BaseErrorController.class)
+class BaseErrorControllerTest {
 
     private final MockMvc mvc;
 
-    public BaseControllerTest(@Autowired MockMvc mvc) {
+    public BaseErrorControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
-    @DisplayName("[view][GET] 기본 페이지 요청")
+    @DisplayName("[view][GET] 에러 페이지 요청")
     @Test
-    void testRoot() throws Exception {
+    void testError() throws Exception {
         //Given
 
 
         //When & Then
-        mvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andExpect(content().string(containsString("This is index")))
+        mvc.perform(get("/wrong-uri"))
+                .andExpect(status().isNotFound())
                 .andDo(print());
-
 
         //Then
 
